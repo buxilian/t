9a4c801c193ae01f88f91683cc949c2592022457
buxilian <97781857+buxilian@users.noreply.github.com>
2022-03-02 12:00:54 +0800

    Update trojan.sh

--- trojan.sh
+++ trojan.sh
@@ -150,13 +150,13 @@ function getData()
         CERT_FILE="/usr/local/etc/trojan/${DOMAIN}.pem"
         KEY_FILE="/usr/local/etc/trojan/${DOMAIN}.key"
     else
-       ## resolve=`curl -sm8 ipget.net/?ip=${DOMAIN}`
+       ## resolve=`curl -sL https://hijk.art/hostip.php?d=${DOMAIN}`
        ## res=`echo -n ${resolve} | grep ${IP}`
         ##if [[ -z "${res}" ]]; then
           ##  echo " ${DOMAIN} 解析结果：${resolve}"
             ## echo -e " ${RED}域名未解析到当前服务器IP(${IP})!${PLAIN}"
-	    resolve=$(curl -sm8 ipget.net/?ip=${DOMAIN})
-	    res=$(echo -n ${resolve} | grep ${IP})
+	    resolve=`curl -sm8 ipget.net/?ip=${DOMAIN}`
+	   # res=`echo -n ${resolve} | grep ${IP}`
 			if [ $resolve != $IP ]; then
 				colorEcho ${BLUE} "${DOMAIN} 解析结果：${resolve}"
 				colorEcho ${RED} " 域名未解析到当前服务器IP(${IP})！"
