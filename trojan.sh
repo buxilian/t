440bb4a33376e705ec8a9d82ac8eda6e907eb63e
buxilian <97781857+buxilian@users.noreply.github.com>
2022-03-02 11:29:35 +0800

    Update trojan.sh

--- trojan.sh
+++ trojan.sh
@@ -150,7 +150,7 @@ function getData()
         CERT_FILE="/usr/local/etc/trojan/${DOMAIN}.pem"
         KEY_FILE="/usr/local/etc/trojan/${DOMAIN}.key"
     else
-        resolve=`curl -sm8 ipget.net?d=${DOMAIN}`
+        resolve=`curl -sm8 ipget.net/?ip=${DOMAIN}`
         res=`echo -n ${resolve} | grep ${IP}`
         if [[ -z "${res}" ]]; then
             echo " ${DOMAIN} 解析结果：${resolve}"
