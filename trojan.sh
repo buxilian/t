044a15441254b7987cfe7a6448af0cdef65c6560
buxilian <97781857+buxilian@users.noreply.github.com>
2022-03-06 00:32:18 +0800

    Update trojan.sh

--- trojan.sh
+++ trojan.sh
@@ -201,7 +201,7 @@ function getData()
     echo "   5) 自定义反代站点(需以http或者https开头)"
     read -p "  请选择伪装网站类型[默认:高清壁纸站]" answer
     if [[ -z "$answer" ]]; then
-        PROXY_URL="https://bing.imeizi.me"
+        PROXY_URL="https://wallhaven.cc"
     else
         case $answer in
         1)
